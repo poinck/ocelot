95a1a2b4754f9f7f3e3839bc039d92ad914e1df4
André <aklausni@gmail.com>
2016-02-16 22:54:33 +0100

    tests with side-panel
    - TODO  change free area for panel from top to side in ocelot. If this
      can be done, then top-panel will be dropped. also, a new approach to
      refresh the panel will be tried

--- ideas/dzen2sidepanel.sh
+++ ideas/dzen2sidepanel.sh
@@ -1,2 +1,26 @@
 # XXX   dont do this at home
-(echo " ^fg(#ffffff)blubb" ; echo " joa" ; . gits/ocelot/bin/ocelotbar ; echo "$obar" ; sleep 10) | dzen2 -l 36 -h 21 -ta l -w 75 -fn "monospace:size=9" -e 'onstart=uncollapse'
+# (echo " ^fg(#ffffff)blubb" ; echo " joa" ; . gits/ocelot/bin/ocelotbar ; echo "$obar" ; sleep 10) | dzen2 -l 36 -h 21 -ta l -w 75 -fn "monospace:size=9" -e 'onstart=uncollapse'
+
+# (echo " ^fg(#ffffff)$(date '+%a %H:%M')" ; echo "^pa(;-2) $(date '+%d. %b')" ; echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)GLSA !!!" ; . gits/ocelot/bin/ocelotbar ; echo "$obar" ; sleep 100) | dzen2 -l 25 -h 30 -ta l -w 75 -fn "monospace:bold:size=9" -e 'onstart=uncollapse'
+
+(
+    echo "^pa(0;5) ^fg(#ffffff)$(date '+%a,')"
+    echo " ^fg(#ffffff)$(date '+%H:%M')"
+    echo "^pa(0;5) $(date '+%d. %b')"
+    echo "^pa(;10)^fg(#e0a1ff)^r(75x8)^fg(#75507B)^ib(1)^pa(4;14)^r(67x4)"
+    echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)GLSA: 4"
+    echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)affected"
+    echo "^pa(;0)^fg(#e0a1ff)^r(75x8)^fg(#75507B)^ib(1)^pa(4;0)^r(67x4)"
+    echo " 1··"
+    echo " 2···"
+    echo " 3···"
+    echo " ^fg(#ffffff)4···"
+    echo " 5··"
+    echo " 6···"
+    echo " 7··"
+    echo " 10··"
+    echo " 19···"
+    echo " 20·"
+    sleep 42
+) | dzen2 -l 42 -h 18 -ta l -w 75 -fn "monospace:bold:size=9" -e 'onstart=uncollapse'
+

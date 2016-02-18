d481189dfcd2424da80c5e398bf05ed06fecc59f
André <aklausni@gmail.com>
2016-02-18 18:02:24 +0100

    improvments for side-panel idea

--- ideas/dzen2sidepanel.sh
+++ ideas/dzen2sidepanel.sh
@@ -4,12 +4,12 @@
 # (echo " ^fg(#ffffff)$(date '+%a %H:%M')" ; echo "^pa(;-2) $(date '+%d. %b')" ; echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)GLSA !!!" ; . gits/ocelot/bin/ocelotbar ; echo "$obar" ; sleep 100) | dzen2 -l 25 -h 30 -ta l -w 75 -fn "monospace:bold:size=9" -e 'onstart=uncollapse'
 
 (
-    echo "^pa(0;5) ^fg(#ffffff)$(date '+%a,')"
-    echo " ^fg(#ffffff)$(date '+%H:%M')"
-    echo "^pa(0;5) $(date '+%d. %b')"
+    echo "^pa(0;4) ^fg(#ffffff)$(date '+%a,') ^fg()$(date '+W%-W')"
+    echo " ^fg(#ffffff)$(date '+%-H:%M')"
+    echo "^pa(0;4) $(date '+%-d. %b')"
     echo "^pa(;10)^fg(#e0a1ff)^r(75x8)^fg(#75507B)^ib(1)^pa(4;14)^r(67x4)"
-    echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)GLSA: 4"
-    echo "^fg(#e0a1ff)^r(75x30)^fg(#75507B)^ib(1)^pa(4)^r(67x22)^pa(8)^fg(#ffffff)affected"
+    echo "^fg(#e0a1ff)^r(75x16)^fg(#75507B)^ib(1)^pa(4)^r(67x16)^pa(8;2)^fg(#ffffff)GLSA: 4"
+    echo "^fg(#e0a1ff)^r(75x16)^fg(#75507B)^ib(1)^pa(4)^r(67x16)^pa(8;2)^fg(#ffffff)affected"
     echo "^pa(;0)^fg(#e0a1ff)^r(75x8)^fg(#75507B)^ib(1)^pa(4;0)^r(67x4)"
     echo " 1··"
     echo " 2···"
@@ -22,5 +22,5 @@
     echo " 19···"
     echo " 20·"
     sleep 42
-) | dzen2 -l 42 -h 18 -ta l -w 75 -fn "monospace:bold:size=9" -e 'onstart=uncollapse'
+) | dzen2 -l 48 -h 16 -ta l -w 75 -fn "monospace:bold:size=9" -e 'onstart=uncollapse' -bg "#0f0f0f"
 
